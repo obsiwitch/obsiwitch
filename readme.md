936e685c2edd93f9b6c06eda64962df0025928f8
Obsidienne <obsiagae@gmail.com>
2019-06-07 00:57:52 +0200

    generate - posts

---
+++ readme.md
@@ -0,0 +1,13 @@
+# [obsidienne.gitlab.io](https://obsidienne.gitlab.io)
+
+## Dependencies
+
+* [pandoc](https://pandoc.org/)
+* [sassc](https://github.com/sass/sassc)
+* [yq](https://github.com/mikefarah/yq)
+
+## Build
+
+~~~sh
+> src/generate.sh [--clean] [--debug]
+~~~
---
+++ src/generate.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+set -o nounset -o errexit -o pipefail
+[[ "$*" == *--debug* ]] && set -x
+
+function generate_posts() {
+    echo '# Generate posts'
+    mkdir -p 'public/posts'
+
+    local list='public/posts/list.yml'
+    echo 'title: posts' > "$list"
+    echo 'post:' >> "$list"
+    for post in posts/*.md; do
+        echo '-' >> "$list"
+        echo " file: /posts/$(basename "${post%.*}.html")" >> "$list"
+        sed -ne '/---/,/---/{p}' "$post" \
+            | sed -e '1d;$d' -e 's/^/ /' \
+            >> "$list"
+    done
+    tmp=$(mktemp)
+    cat "$list" \
+        | yq '.post |= sort_by(.date)' \
+        | yq '.post |= reverse' \
+        | yq '.' --yml-output \
+        > "$tmp"
+    mv "$tmp" "$list"
+
+    for post in posts/*.md; do
+        pandoc "$post" \
+            --output="public/posts/$(basename "${post%.*}.html")" \
+            --standalone --template='templates/layout.html' \
+            --toc --number-sections
+    done
+}
+
+function generate_pages() {
+    echo '# Generate pages'
+    mkdir -p 'public'
+
+    [[ -f 'public/posts/list.yml' ]] || (
+        mkdir -p 'public/posts'
+        touch 'public/posts/list.yml'
+    )
+    pandoc '/dev/null' \
+        --output='public/posts.html' \
+        --template='templates/posts.html' \
+        --metadata-file='public/posts/list.yml'
+
+    pandoc 'pages/index.md' \
+        --output='public/index.html' \
+        --standalone --template='templates/layout.html' \
+        --include-after-body='public/posts.html'
+}
+
+function generate_assets() {
+    echo '# Generate assets'
+    mkdir -p 'public'
+
+    cp -r 'assets' 'public'
+    sassc --style compressed 'assets/style.scss' 'public/assets/style.css'
+}
+
+[[ "$*" == *--clean* ]] && rm -rf 'public'
+
+generate_posts
+generate_pages
+generate_assets
+
+[[ "$*" == *--debug* ]] && {
+    python -m http.server 8000 \
+           --bind 'localhost' \
+           --directory 'public/'
+}
+
+exit 0
